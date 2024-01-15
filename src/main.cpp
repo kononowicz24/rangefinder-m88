1064736b14f509575bc6fbb5577684cf4c64b998
kononowicz24 <kononowicz24@gmail.com>
2024-01-16 00:10:30 +0100

    changed ultrasonic library

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <Ultrasonic.h>
+#include <HCSR04.h>
 #include <U8g2lib.h>
 
 // put function declarations here:
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0); 
-Ultrasonic ultrasonic(0,1 , 20000UL);
+byte triggerPin = 4;
+byte echoPin = 5;
 
-int len_us_cm = 0;
+double len_us_cm;
 
 void u8g2_update();
 void ultrasonic_update();
@@ -15,6 +16,7 @@ void ultrasonic_update();
 void setup() {
   // put your setup code here, to run once:
   u8g2.begin(); 
+  HCSR04.begin(triggerPin, echoPin);
 }
 
 void loop() {
@@ -27,7 +29,6 @@ void u8g2_update() {
   u8g2.firstPage();
 		do {
 			u8g2.setFont(u8g2_font_logisoso24_tn  );
-			u8g2.drawStr(0,8,"t: ");
 			u8g2.setCursor(18,24);
 			u8g2.print(len_us_cm);
 			//u8g2.drawStr(80,24,"2.8");
@@ -36,5 +37,6 @@ void u8g2_update() {
 }
 
 void ultrasonic_update() {
-  len_us_cm = ultrasonic.read();
+    double* distances = HCSR04.measureDistanceCm();
+    len_us_cm = distances[0];
 }
